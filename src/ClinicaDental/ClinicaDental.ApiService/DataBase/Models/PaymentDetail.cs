9a68ef15ee2b9dba3728708457194a4f753bb7e9
ReynaldoCoMa <reynaldomartin57@gmail.com>
2024-11-09 20:14:48 -0600

    Create PaymentDetail.cs

---
+++ src/ClinicaDental/ClinicaDental.ApiService/DataBase/Models/PaymentDetail.cs
@@ -0,0 +1,17 @@
+﻿namespace ClinicaDental.ApiService.DataBase.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class PaymentDetail
+{
+    [Key]
+    public int paymentDetailId { get; set; }
+
+    public string cardOwnerName { get; set; }
+
+    public string cardNumber { get; set; }
+
+    public string expirationDate { get; set; }
+
+    public string securityCode { get; set; }
+}
