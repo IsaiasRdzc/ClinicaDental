f60a20b7f1bd1706dccd0dfd6835cdcebebcb972
Tyrone J <tyronejjd24@gmail.com>
2024-11-20 14:57:55 -0600

    Refactor ErrorHandling
    Error handling converted in to a static class for reuse since static classes (endpoints) cant use interfaces

---
+++ src/ClinicaDental/ClinicaDental.ApiService/MedicalRecords/Services/ErrorOrResultHandler.cs
@@ -0,0 +1,46 @@
+﻿namespace ClinicaDental.ApiService.MedicalRecords.Services;
+
+public static class ErrorOrResultHandler
+{
+    public static async Task<IResult> HandleRequest(Func<Task> operation)
+    {
+        try
+        {
+            await operation();
+            return Results.Ok();
+        }
+        catch (ArgumentException error)
+        {
+            return Results.BadRequest(error.Message);
+        }
+        catch (KeyNotFoundException error)
+        {
+            return Results.NotFound(error.Message);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Results.Unauthorized();
+        }
+    }
+
+    private static async Task<IResult> HandleRequest<T>(Func<Task<T>> operation)
+    {
+        try
+        {
+            var result = await operation();
+            return Results.Ok(result);
+        }
+        catch (ArgumentException error)
+        {
+            return Results.BadRequest(error.Message);
+        }
+        catch (KeyNotFoundException error)
+        {
+            return Results.NotFound(error.Message);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            return Results.Unauthorized();
+        }
+    }
+}
--- src/ClinicaDental/ClinicaDental.ApiService/MedicalRecords/Services/MedicalInformationEndpoints.cs
+++ src/ClinicaDental/ClinicaDental.ApiService/MedicalRecords/Services/MedicalInformationEndpoints.cs
@@ -2,8 +2,6 @@
 
 using ClinicaDental.ApiService.DataBase.Models.MedicalRecords;
 
-using Microsoft.EntityFrameworkCore;
-
 public static class MedicalInformationEndpoints
 {
     public static void MapMedicalRecordsEndpoints(this IEndpointRouteBuilder app)
@@ -22,73 +20,31 @@ public static class MedicalInformationEndpoints
 
     public static async Task<IResult> CreateRecord(MedicalRecord medicalRecord, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.SaveMedicalRecord(medicalRecord));
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.SaveMedicalRecord(medicalRecord));
     }
 
     public static async Task<IResult> UpdateRecord(int medicalRecordId, MedicalRecord medicalRecord, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.UpdateMedicalRecord(medicalRecordId, medicalRecord));
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.UpdateMedicalRecord(medicalRecordId, medicalRecord));
     }
 
     public static async Task<IResult> DeleteMedicalRecordByRecordId(int medicalRecordId, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.DeleteMedicalRecordByRecordId(medicalRecordId));
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.DeleteMedicalRecordByRecordId(medicalRecordId));
     }
 
     public static async Task<IResult> SearchRecordById(int medicalRecordId, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.SearchMedicalRecordByRecordId(medicalRecordId));
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.SearchMedicalRecordByRecordId(medicalRecordId));
     }
 
     public static async Task<IResult> SearchRecordsByPatientId(int patientId, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.SearchMedicalRecordsByPatientId(patientId));
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.SearchMedicalRecordsByPatientId(patientId));
     }
 
     public static async Task<IResult> SearchRecordsByDoctorId(int doctorId, MedicalInformationManager medicalRecordsManager)
     {
-        return await HandleErrorOrResult(async () => await medicalRecordsManager.SearchMedicalRecordsByDoctorId(doctorId));
-    }
-
-    public static async Task<IResult> HandleErrorOrResult(Func<Task> operation)
-    {
-        try
-        {
-            await operation();
-            return Results.Ok();
-        }
-        catch (ArgumentException error)
-        {
-            return Results.BadRequest(error.Message);
-        }
-        catch (KeyNotFoundException error)
-        {
-            return Results.NotFound(error.Message);
-        }
-        catch (UnauthorizedAccessException)
-        {
-            return Results.Unauthorized();
-        }
-    }
-
-    private static async Task<IResult> HandleErrorOrResult<T>(Func<Task<T>> operation)
-    {
-        try
-        {
-            var result = await operation();
-            return Results.Ok(result);
-        }
-        catch (ArgumentException error)
-        {
-            return Results.BadRequest(error.Message);
-        }
-        catch (KeyNotFoundException error)
-        {
-            return Results.NotFound(error.Message);
-        }
-        catch (UnauthorizedAccessException)
-        {
-            return Results.Unauthorized();
-        }
+        return await ErrorOrResultHandler.HandleRequest(async () => await medicalRecordsManager.SearchMedicalRecordsByDoctorId(doctorId));
     }
 }
