68d2527de9cefc4b6eb09256824a5a0ea2a2a21c
Tyrone J <tyronejjd24@gmail.com>
2024-11-21 03:26:26 -0600

    minor fixes in programCs

--- src/ClinicaDental/ClinicaDental.ApiService/Program.cs
+++ src/ClinicaDental/ClinicaDental.ApiService/Program.cs
@@ -45,9 +45,9 @@ builder.Services.AddTransient<ClinicReceptionist>();
 builder.Services.AddTransient<ClinicAgenda>();
 builder.Services.AddTransient<ClinicAdmin>();
 builder.Services.AddTransient<PaymentsAdmin>();
-builder.Services.AddTransient<MedicalInformationManager>();
 builder.Services.AddTransient<AccountsManager>();
 builder.Services.AddTransient<MedicalRecordsManager>();
+builder.Services.AddTransient<PatientsInformationManager>();
 
 // Registries
 builder.Services.AddTransient<SuppliesRegistry>();
