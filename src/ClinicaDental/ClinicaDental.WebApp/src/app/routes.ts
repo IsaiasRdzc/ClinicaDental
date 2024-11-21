b71590906b175fb30148c91afbb3244e2c422abc
ReynaldoCoMa <reynaldomartin57@gmail.com>
2024-11-21 13:31:12 -0600

    inventorycomponent

--- src/ClinicaDental/ClinicaDental.WebApp/src/app/routes.ts
+++ src/ClinicaDental/ClinicaDental.WebApp/src/app/routes.ts
@@ -2,6 +2,7 @@ import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { PaymentDetailsComponent } from "./payment-details/payment-details.component";
 import { LoginComponent } from "./login/login/login.component";
+import { InventorySectionComponent } from "./inventory/inventory-section/inventory-section.component";
 
 const routeConfig: Routes=[
     {
@@ -18,6 +19,11 @@ const routeConfig: Routes=[
         path: "login",
         component: LoginComponent,
         title: "Dentist Login"
+    },
+    {
+        path: "inventory-section",
+        component: InventorySectionComponent,
+        title: "Inventory"
     }
    
 ];
